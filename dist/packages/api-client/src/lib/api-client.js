58edac7578aad0c35025a615c18ac2bff8f2a2b6
Jose Badeau <jose.badeau@juliusbaer.com>
2026-05-15 14:14:44 +0200

    fabster: generate-library — Generate a library using an Nx generator

---
+++ dist/packages/api-client/src/lib/api-client.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.apiClient = apiClient;
+function apiClient() {
+    return 'api-client';
+}
+//# sourceMappingURL=api-client.js.map
