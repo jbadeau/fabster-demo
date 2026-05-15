b2c6c8e7bc0f56de2d9f12e563b9f7471216dd18
Jose Badeau <jose.badeau@juliusbaer.com>
2026-05-15 09:49:37 +0200

    fabster: generate-library — Generate a library using an Nx generator

---
+++ dist/packages/api-spec/src/lib/api-spec.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.apiSpec = apiSpec;
+function apiSpec() {
+    return 'api-spec';
+}
+//# sourceMappingURL=api-spec.js.map
